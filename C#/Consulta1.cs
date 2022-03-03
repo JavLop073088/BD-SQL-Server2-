27ed4f939b7fd2cc0a890b77cd826d86679b7613
Javier López <92817668+JavLop073088@users.noreply.github.com>
2022-03-03 18:40:47 -0300

    Add files via upload

---
+++ C#/Consulta1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TecnicaturaBackend.Dominio
+{
+    public class Consulta1
+    {
+        public int valorInt1 { get; set; }
+        public int valorInt2 { get; set; }
+        public int valorInt3 { get; set; }
+        public int valorInt4 { get; set; }
+
+        public int valorInt5 { get; set; }
+        public string valorString1 { get; set; }
+        public string valorString2 { get; set; }
+        public string valorString3 { get; set; }
+        public string valorString4 { get; set; }
+        public string valorString5 { get; set; }
+    }
+}
